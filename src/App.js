4dbcbfbad0b060105b5c2feee4f00a85de05d327
SKallDay <shaistakirmani@gmail.com>
2021-01-10 15:13:54 -0500

    refine layout and component styles

--- src/App.js
+++ src/App.js
@@ -8,64 +8,30 @@ import './App.css';
 function App() {
   const [nominations, setNominations] = useState([]);
   const [query, setQuery] = useState("");
-  // const [queryResults, setQueryResults] = useState([]);
+  const [queryResults, setQueryResults] = useState([]);
 
   const APP_KEY = "97b773a7";
   const url = `http://www.omdbapi.com/?apikey=${APP_KEY}&s=${query}`;
 
-  // useEffect(() => {
-  //   fetchMovies();
-  // }, [query]);
+  useEffect(() => {
+    fetchMovies();
+  }, [query]);
 
-  // const fetchMovies = async () => {
-  //   const response = await fetch(url);
-  //   const data = await response.json();
-  //   setQueryResults(data.Search);
-  // };
-
-  const queryResults = [
-    {
-      Title: "harry potter - test 1",
-      Year: "2001",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-    {
-      Title: "harry potter test 2 ",
-      Year: "2001",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-    {
-      Title: "harry potter test 3",
-      Year: "2001",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-    {
-      Title: "harry potter - test 4",
-      Year: "2001",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-    {
-      Title: "harry potter - test 5",
-      Year: "2001",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-  ];
-
-
+  const fetchMovies = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setQueryResults(data.Search);
+  };
 
   const queryBySearch = (value) => {
     setQuery(value);
   };
 
   const addToNominations = (item) => {
-    // add conditional if nominations less then 5 add
-    const Noms = [...nominations, item];
-    setNominations(Noms);
+    if (nominations.length < 5) {
+      const Noms = [...nominations, item];
+      setNominations(Noms);
+    }
   };
 
   const removeNominee = (item) => {
